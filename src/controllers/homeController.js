417c74489fe42d3b003d62feca18030d6cd6a866
Nguyen Minh Duc <nguyenminhduc06072002@gmail.com>
2024-11-07 15:41:47 +0700

    CRUD User Model

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,9 @@
 import db from '../models/index'
+import CRUDService from '../services/CRUDService'
 
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll()
-        console.log('--------------------------------')
-        console.log(data)
-        console.log('--------------------------------')
         return res.render('homepage.ejs', {
             data: JSON.stringify(data)
         })
@@ -14,6 +12,60 @@ let getHomePage = async (req, res) => {
     }
 }
 
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs')
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body)
+    console.log(message)
+    return res.send('POST CRUD')
+}
+
+let displayGetCRUD = async (req, res) => {
+    let data = await CRUDService.getAllUser()
+    return res.render("displayCRUD.ejs", {
+        dataTable: data
+    })
+}
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id
+    if (userId) {
+        let userData = await CRUDService.getUserInfoByID(userId)  
+
+        return res.render('editCRUD.ejs', {
+            user: userData  
+        })
+    } else {
+        return res.send('User ID not found')
+    }
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body
+    let allUsers = await CRUDService.updateUserData(data)
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers  
+    })
+}
+
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id
+    if (id) {
+        await CRUDService.deleteUserById(id)
+        return res.send('DELETE USER SUCCESSFULLY')
+    } else {
+        return res.send('User ID not found')
+    }
+}
+
 module.exports = {
     getHomePage,
+    getCRUD,
+    postCRUD,
+    displayGetCRUD,
+    getEditCRUD,
+    putCRUD,
+    deleteCRUD
 }
