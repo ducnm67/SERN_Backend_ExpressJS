50399e15904d819c9a3e2b13597982db07fb7454
Nguyen Minh Duc <nguyenminhduc06072002@gmail.com>
2024-11-09 17:38:53 +0700

    Complete basic CRUD functions for User model

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,18 @@
 import bcrypt from 'bcryptjs';
 import db from '../models/index'
-import { raw } from 'body-parser';
+
+const salt = bcrypt.genSaltSync(10);
+
+let hashUserPassword = (password) => {
+    return new Promise(async (resolve, reject) => {
+        try{
+            let hashPassword = await bcrypt.hashSync(password, salt); 
+            resolve(hashPassword);
+        }catch(e) {
+            reject(e);
+        }   
+    })
+}
 
 let handleUserLogin = (email, password) => {
     return new Promise(async (resolve, reject) => {
@@ -56,7 +68,125 @@ let checkUserEmail = (userEmail) => {
     })
 }
 
+let getAllUsers = async (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let users = ''
+            if (userId === 'ALL') {
+                users = await db.User.findAll({
+                    attributes: {
+                        exclude: ['password']
+                    }
+                })
+            } 
+            if (userId && userId !== 'ALL') {
+                users = db.User.findOne({
+                    where: {id: userId},
+                    attributes: {
+                        exclude: ['password']
+                    }
+                })
+            }
+            resolve(users)
+        } catch (error) {
+            reject(error)
+        }
+    })
+
+}
+
+let createNewUser = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let check = await checkUserEmail(data.email)
+            if(check !== true) {
+                let hashPasswordFromBcrypt = await hashUserPassword(data.password)
+                await db.User.create({
+                    email: data.email,
+                    password: hashPasswordFromBcrypt,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    address: data.address,
+                    phonenumber: data.phonenumber,
+                    gender: data.gender === '1'? true: false,
+                    roleld: data.roleId
+                })
+                resolve({
+                    errCode: 0,
+                    errMessage: 'ok'
+                })
+            } else {
+                resolve({
+                    errCode: 1,
+                    message: 'Your email is already in used, Plz try another email!'
+                })
+            }
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+let deleteUser = (id) => {
+    return new Promise(async (resolve, reject) => {
+        let user = await db.User.findOne({
+            where: {id: id},
+        })
+        if(!user) {
+            resolve({
+                errCode: 2,
+                errMessage: `The user isn't exist`
+            })
+        } 
+        await db.User.destroy({
+            where: {id: id},
+        })
+        resolve({
+            errCode: 0,
+            errMessage: `The user is deleted`
+        })
+    })
+}
+
+let updateUserData = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if(!data.id){
+                resolve({
+                    errCode: 2,
+                    message: 'User ID is required'
+                })
+                return;
+            }
+            let user = await db.User.findOne({
+                where: {id: data.id},
+                raw: false,
+            })
+            if (user) {
+                user.firstName = data.firstName;
+                user.lastName = data.lastName;
+                user.address = data.address;
+                await user.save();
+                resolve({
+                    errCode: 0,
+                    message: 'Update the user succeeds!'
+                })
+            } else {
+                resolve({
+                    errCode: 1,
+                    message: `User's not found`
+                })
+            } 
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
 module.exports = {
     handleUserLogin,
-    checkUserEmail
+    getAllUsers,
+    createNewUser,
+    deleteUser,
+    updateUserData
 }
