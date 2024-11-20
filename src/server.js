0bdea4bda063f2303b8477f7196c0061243e24cb
Nguyen Minh Duc <nguyenminhduc06072002@gmail.com>
2024-11-21 01:07:30 +0700

    Edit api updateUser, reconfigure server and User modal to complete image upload and update function

--- src/server.js
+++ src/server.js
@@ -1,26 +1,26 @@
-import express from "express";
-import bodyParser from "body-parser";
-import viewEngine from "./config/viewEngine";
-import initWebRoutes from "./routes/web";
-import connectDB from "./config/connectDB"
-import cors from "cors"
+import express from 'express';
+import bodyParser from 'body-parser';
+import viewEngine from './config/viewEngine';
+import initWebRoutes from './routes/web';
+import connectDB from './config/connectDB';
+import cors from 'cors';
 
-require('dotenv').config()
+require('dotenv').config();
 
 let app = express();
 app.use(cors({ credentials: true, origin: true }));
 
 //config app
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
 
-connectDB()
+connectDB();
 
-let port = process.env.PORT || 6969
+let port = process.env.PORT || 6969;
 
 app.listen(port, () => {
-    console.log("Backend Nodejs is runing on the port: " + port)
+    console.log('Backend Nodejs is runing on the port: ' + port);
 });
--- src/services/userService.js
+++ src/services/userService.js
@@ -120,6 +120,7 @@ let createNewUser = (data) => {
                     gender: data.gender,
                     roleId: data.roleId,
                     positionId: data.positionId,
+                    image: data.avatar,
                 });
                 resolve({
                     errCode: 0,
@@ -162,7 +163,7 @@ let deleteUser = (id) => {
 let updateUserData = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!data.id) {
+            if (!data.id || !data.roleId || !data.positionId || !data.gender) {
                 resolve({
                     errCode: 2,
                     message: 'User ID is required',
@@ -177,6 +178,14 @@ let updateUserData = (data) => {
                 user.firstName = data.firstName;
                 user.lastName = data.lastName;
                 user.address = data.address;
+                user.roleId = data.roleId;
+                user.positionId = data.positionId;
+                user.gender = data.gender;
+                user.phonenumber = data.phonenumber;
+                if (data.avatar) {
+                    user.image = data.avatar;
+                }
+
                 await user.save();
                 resolve({
                     errCode: 0,
